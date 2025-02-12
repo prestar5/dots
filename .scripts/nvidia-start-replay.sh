82b3c3442eeade594629b4071811d853c9225eb6
stinker <xsaustra@proton.me>
2025-02-12 21:11:20 +0000

    Scripts - Change default microphone.

--- .scripts/nvidia-start-replay.sh
+++ .scripts/nvidia-start-replay.sh
@@ -3,4 +3,4 @@
 killall -SIGINT gpu-screen-recorder && sleep 0.5 && notify-send -t 1500 -u low 'GPU Screen Recorder' 'Stopped replay buffer.' --icon=record && exit 0
 video="$HOME/Videos/Recording"
 notify-send -t 5000 -u low 'GPU Screen Recorder' "Started replay buffer. (Press ALT+F8 to save clips.)" --icon=record
-gpu-screen-recorder -w screen -f 60 -q ultra -a "default_output|default_input" -c mp4 -r 180 -o "$video"
+gpu-screen-recorder -w screen -f 60 -q ultra -a "default_output|alsa_input.usb-SteelSeries_SteelSeries_Alias-00.mono-fallback" -c mp4 -r 180 -o "$video"
--- .scripts/nvidia-toggle-record.sh
+++ .scripts/nvidia-toggle-record.sh
@@ -3,4 +3,4 @@
 killall -SIGINT gpu-screen-recorder && sleep 0.5 && notify-send -t 1500 -u low 'GPU Screen Recorder' 'Stopped recording.' --icon=record && exit 0
 video="$HOME/Videos/Recording/$(date +"Video_%Y-%m-%d_%H-%M-%S.mp4")"
 notify-send -t 5000 -u low 'GPU Screen Recorder' "Started recording video to $video" --icon=record
-gpu-screen-recorder -w screen -c mp4 -f 60 -bm qp -q ultra -a "default_output|default_input" -o "$video"
+gpu-screen-recorder -w screen -c mp4 -f 60 -bm qp -q ultra -a "default_output|alsa_input.usb-SteelSeries_SteelSeries_Alias-00.mono-fallback" -o "$video"
